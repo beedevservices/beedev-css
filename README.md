2388975bf5cada2828a1cb6df73e2ebc79237315
melissa-24 <melissalongenberger24@gmail.com>
2021-09-27 00:36:32 -0400

    adding hacktoberfest files

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # beedev-css
 
+
+# PURPOSE:
 A more personal version of a bootstrap style css.  Deployed through github pages and open to be used by anyone.  Got something you want to add or see something that needs to be fixed?  Feel free to add your changes as a pull request.
+
+# PROGRESS:
+Currently is deployed and has some of the basics of the styles up.
+
+## WHAT IS NEEDED:
+
+1. Documentation to what the classes are
+2. Create search functions for the documentation
+3. Create Templates using the styles
+
+## REACH ME OR CHAT WITH ME:
+* Email: melissa@beedev-services.com
+* Slack: https://join.slack.com/t/beedev-workspace/shared_invite/zt-w8zrxgiu-G4jRerfz0JcboaX~qdnDvg
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function copyText() {
+function copyCssText() {
     var copyText = document.querySelector('#cssLink')
     console.log(copyText)
     copyText.select()
@@ -6,3 +6,53 @@ function copyText() {
     navigator.clipboard.writeText(copyText.value)
     alert("Copied the text: " + copyText.value)
   }
+  function copyJsText() {
+    var copyText = document.querySelector('#jsLink')
+    console.log(copyText)
+    copyText.select()
+    copyText.setSelectionRange(0, 99999)
+    navigator.clipboard.writeText(copyText.value)
+    alert("Copied the text: " + copyText.value)
+  }
+
+const root = document.documentElement 
+const themeBtns = document.querySelectorAll('.theme > button')
+console.log("js connected")
+
+themeBtns.forEach((btn) => {
+  btn.addEventListener('click', handleThemeUpdate)
+})
+
+function handleThemeUpdate(e) {
+  switch(e.target.value) {
+    case 'dark': 
+        root.style.setProperty('--bg', '#000000')
+        root.style.setProperty('--bg-text', '#ffffff')
+        break
+    case 'green': 
+       root.style.setProperty('--bg', '#006600')
+       root.style.setProperty('--bg-text', '#e6ffe6')
+       break
+    case 'blue':
+        root.style.setProperty('--bg', '#003399')
+        root.style.setProperty('--bg-text', '#e6eeff')
+        break
+    case 'purple':
+        root.style.setProperty('--bg', '#993399')
+        root.style.setProperty('--bg-text', '#f9ecf9')
+        break
+    case 'cyan':
+        root.style.setProperty('--bg', '#009999')
+        root.style.setProperty('--bg-text', '#e6ffff')
+        break
+    case 'red':
+        root.style.setProperty('--bg', '#e60000')
+        root.style.setProperty('--bg-text', '#ffe6e6')
+        break
+    case 'light':
+        root.style.setProperty('--bg', '#ffffff')
+        root.style.setProperty('--bg-text', '#000000')
+        break
+  }
+  console.log(e.target.value)
+}
